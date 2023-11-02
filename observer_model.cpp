9003eaf487ddd7bcb7f7fb69d26af95dc53f7ca9
TeTng <zjteng98@gmail.com>
2023-11-02 16:17:39 +0800

    observer_model

--- observer_model.cpp
+++ observer_model.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "observer_model.h"
+#include "iostream"
+
+using namespace std;
+
+void UserMessageObserver::update() {
+    cout << "UserMessageObserver::update()" << endl;
+    if (wallet_->getBalance() <= 5.) {
+        cout << "wallet_->getBalance() <= 5." << endl;
+    }
+}
+
+UserMessageObserver::UserMessageObserver(UserWallet *wallet) {
+    wallet_ = wallet;
+    wallet->attach_observer(this);
+}
+
+void UserWallet::attach_observer(Observer *observer) {
+    observer_vec_.push_back(observer);
+}
+
+void UserWallet::deposit(float p) {
+    balance_ += p;
+}
+
+void UserWallet::consume(float p) {
+    balance_ -= p;
+    notify();
+}
+
+float UserWallet::getBalance() {
+    return balance_;
+}
+
+void UserWallet::notify() {
+    for (auto ob: observer_vec_) {
+        ob->update();
+    }
+}
--- observer_model.h
+++ observer_model.h
@@ -5,4 +5,37 @@
 #ifndef DESIGN_MODEL_OBSERVER_MODEL_H
 #define DESIGN_MODEL_OBSERVER_MODEL_H
 
+#include "vector"
+
+class Observer {
+public:
+    virtual void update() = 0;
+};
+
+class UserWallet {
+public:
+    void attach_observer(Observer *observer);
+
+    void deposit(float p);
+
+    void consume(float p);
+
+    void notify();
+
+    float getBalance();
+
+    float balance_ = 0.;
+
+    std::vector<Observer *> observer_vec_;
+};
+
+class UserMessageObserver : public Observer {
+public:
+    explicit UserMessageObserver(UserWallet *wallet);
+
+    void update() override;
+
+    UserWallet *wallet_ = nullptr;
+};
+
 #endif //DESIGN_MODEL_OBSERVER_MODEL_H
